b8a47cfd0c6491110b682a62ac5935ac92a30a6d
MDyle20 <md.echelon@gmail.com>
2024-06-17 21:35:15 +0100

    Ndryshimi i permbajtjes me shume

--- src/components/me_shume/meshume.js
+++ src/components/me_shume/meshume.js
@@ -4,7 +4,7 @@ export default function Meshume() {
   return <section id="meshume" class="about">
   <img src={require("../../images/veshkaushi.jpg")} alt="Gabimet e Veshkaushit" title="Gabimet e Veshkaushit" class="round" />
   <h3>Më Shume Rreth Vjersha Shqiptare</h3>
-  <span> Kjo faqe është ndërtuar nga një inxhiner software për të festuar të qënit nënë, por edhe te qënit fëmijë në Shqipëri. <br />
+  <span> Kjo faqe është ndërtuar për të festuar të qënit nënë, por edhe te qënit fëmijë në Shqipëri. <br />
     Aktualisht nuk ekziston një regjistër online që përfshin të gjithë vjershat shqiptare kushtuar fëmijëve.<br />
     Kjo faqe synon të përmbledh vjershat për fëmijë por sigurisht pa dashur të dhunojë të drejtat e autorit. Faqa është në punim e sipër.</span>
   <br />
