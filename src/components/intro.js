139aa19a0c88d6e965eec732aebacfdc3ef43149
MDyle20 <md.echelon@gmail.com>
2025-02-27 22:02:04 +0000

    video IOS no popup

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { GatsbyImage } from 'gatsby-plugin-image'
-import VideoThumbnail from 'react-video-thumbnail';
+//import VideoThumbnail from 'react-video-thumbnail';
 
 import * as styles from './intro.module.css'
 
