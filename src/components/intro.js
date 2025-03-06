57881c00f20028ec0a7af7d6dae8b085f89225db
MDyle20 <md.echelon@gmail.com>
2025-03-06 21:05:48 +0000

    video ios

--- src/components/intro.js
+++ src/components/intro.js
@@ -19,7 +19,8 @@ const Intro = ({ image, videoUrl, title, content }) => (
 //                  title="video"
 //                ></iframe></div>
                 <div className={styles.image}>
-                        <video width="100%" height="100%" src={videoUrl} controls  playsInline autostart autoPlay controlsList="nodownload">
+                        <video width="100%" height="100%"  controls  playsInline autoPlay controlsList="nodownload">
+                          <source type="video/mp4" src={videoUrl}/>
                          <track kind="captions" srcLang="en" label="english_captions" />
                          </video>
                         </div>
